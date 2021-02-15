cd6166e9f3c5b4350f1f7743dfc68f3ea1ccfe37
sarwar_sikder <amin.se.aiub@gmail.com>
2021-02-15 10:59:52 +0600

    php basic

---
+++ php/test.php
@@ -0,0 +1,81 @@
+<?php
+
+	//output
+
+	//print "HELLO from PHP";
+	//print('Hello PHP');
+	//echo "hello php"." test...";
+
+	/*$a 		= 10;
+	$name 	= "abc";
+	$cgpa 	= 2.4000007785;*/
+
+
+	//$student = array(1, 'xyz', 3.5, 'xyz@abc.com');
+	/*$student = [1, 'xyz', 3.5, 'xyz@abc.com'];
+	echo $student[3];*/
+
+	/*$students = [ 
+					[1, 'abc', 3.0, 'abc@gmail.com'], 
+					[2, 'xyz', 2.0, 'xyz@gmail.com'], 
+					[3, 'pqr', 4.0, 'pqr@gmail.com'] 
+				];
+
+	echo $students[2][3];*/
+
+/*	$student = ['id'=>1, 'name'=>'xyz', 'cgpa'=>3.5, 'email'=>'xyz@abc.com'];
+	echo $student['email'];*/
+
+
+	/*$students = [ 
+					's1'=> ['id'=>1, 'name'=>'xyz', 'cgpa'=>3.5, 'email'=>'xyz@abc.com'], 
+					's2'=> ['id'=>2, 'name'=>'abc', 'cgpa'=>2.5, 'email'=>'abc@abc.com'], 
+					's3'=> ['id'=>3, 'name'=>'pqr', 'cgpa'=>4.0, 'email'=>'pqr@abc.com'] 
+				];
+
+	echo $students['s1']['name'];*/
+
+	/*$abc = 15;
+
+	if($abc === '15'){
+		echo "true";
+	}else{
+		echo "false";
+	}*/
+
+	/*$student = [1, 'xyz', 3.5, 'xyz@abc.com'];
+
+	for ($i=0; $i < count($student); $i++) { 
+		echo $student[$i];
+		echo " | ";
+	}*/
+
+	/*$student = ['id'=>1, 'name'=>'xyz', 'cgpa'=>3.5, 'email'=>'xyz@abc.com'];
+
+	foreach( $student as $val){
+		echo $val." | ";
+	}*/
+
+	/*function sum($a=0, $b=0){
+		return $a+$b;
+	}
+
+	echo sum(5);*/
+
+/*	$myfile = fopen('abc.txt', 'r');
+	$data = fread($myfile, filesize('abc.txt'));
+	echo $data;
+	fclose($myfile);*/
+
+	/*$myfile = fopen('abc.txt', 'w');
+	fwrite($myfile, 'this is file write example...');
+	echo "Done!";
+	fclose($myfile);*/
+
+	/*$myfile = fopen('abc.txt', 'a');
+	fwrite($myfile, 'this is another file append example...');
+	echo "Done!";
+	fclose($myfile);*/
+
+	echo "<h1>HELLO WORLD ...<br> From PHP</h1>";
+?>
