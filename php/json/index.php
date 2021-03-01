94e31e3fa8d67f2f59d7532e5831506270fb9f72
sarwar_sikder <amin.se.aiub@gmail.com>
2021-03-01 10:26:40 +0600

    json read+write

---
+++ php/json/index.php
@@ -0,0 +1,24 @@
+<?php
+
+	//$myfile = fopen('users.json', 'r');
+	//$data 	= fread($myfile, filesize('users.json'));
+	
+/*	$user = '{
+				"name": "alamin", 
+				"id": 12,
+				"email": "alamin@gmail.com" 
+			}';
+
+	$json 	= json_decode($user, true);
+	print_r($json['name']);*/
+
+	/*$user = [
+				'name' => 'alamin',
+				'id'=>	12,
+				'email' => 'alamin@gmail.com'
+			];
+
+	$json = json_encode($user);
+	echo $json;*/
+
+?>
