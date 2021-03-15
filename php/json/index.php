6be3d7b75c4eb8b01d1282d1f267e094ec7e1a09
sarwar_sikder <amin.se.aiub@gmail.com>
2021-03-15 09:02:54 +0600

    json data read+write

--- php/json/index.php
+++ php/json/index.php
@@ -1,24 +1,30 @@
 <?php
 
-	//$myfile = fopen('users.json', 'r');
-	//$data 	= fread($myfile, filesize('users.json'));
+	$myfile = fopen('users.json', 'r');
+	$data 	= fread($myfile, filesize('users.json'));
 	
-/*	$user = '{
+	/*$user = '{
 				"name": "alamin", 
 				"id": 12,
 				"email": "alamin@gmail.com" 
-			}';
+			}';*/
 
-	$json 	= json_decode($user, true);
-	print_r($json['name']);*/
+	$json 	= json_decode($data, true);
 
-	/*$user = [
+	$user = [
 				'name' => 'alamin',
 				'id'=>	12,
 				'email' => 'alamin@gmail.com'
 			];
 
-	$json = json_encode($user);
-	echo $json;*/
+	array_push($json, $user);
+	
+	//$json = json_encode($user);
+	//print_r($json);
 
+	$data = json_encode($json);
+	echo $data;
+
+	$myfile = fopen('users.json', 'w');
+	fwrite($myfile, $data);
 ?>
